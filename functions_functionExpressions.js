eac5a4f502f222abeb103a5037dc9396c0dbd0e9
Alberto Cristancho <alberto@codeelement.com>
2018-05-26 15:21:40 -0400

    Example files from 'Node.js' Udemy course

--- functions_functionExpressions.js
+++ functions_functionExpressions.js
@@ -24,4 +24,18 @@ logGreeting(function() {
 	console.log('Hello Tony!');
 });
 
+// immediately invoked function expression
+var firstname = 'Jane';
+console.log(firstname);
 
+(function (lastname) {
+	var firstname = 'John';
+	console.log(firstname);
+	console.log(lastname);
+}('Doe'));
+
+/*
+require is a function that you pass a 'path' into
+module.exports is what the require function returns
+this works because your code is actually wrapped in a function that is givne these things as function parameters
+*/
