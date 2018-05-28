cc0cef7c4cabe7dd9b07807e6c72efb155e480b8
Alberto Cristancho <alberto@codeelement.com>
2018-05-28 14:34:05 -0400

    Added more modules and code from 'Node.js' Udemy course

--- app.js
+++ app.js
@@ -1,3 +1,108 @@
+////////// 36 /////////
+
+var person = {
+	firstname: '',
+	lastname: '',
+	greet: function () {
+		return 'Hello, ' + this.firstname + ' ' + this.lastname
+	}
+}
+
+var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Doe';
+
+var jane = Object.create(person);
+jane.firstname = 'Jane';
+jane.lastname = 'Doe';
+
+console.log(john.greet());
+console.log(jane.greet());
+
+/*
+
+
+var eventConfig = require('./config').events;
+var Emitter = require('events');
+
+var emtr = new Emitter();
+
+emtr.on(eventConfig.GREET, function () {
+	console.log('Somewhere, someone says hello.');
+});
+
+emtr.on(eventConfig.GREET, function () {
+	console.log('A greeting occurred!');
+});
+
+console.log('Hello!');
+emtr.emit(eventConfig.GREET);
+
+
+
+
+
+var Emitter = require('./emitter');
+var emtr = new Emitter();
+
+emtr.on('greet', function () {
+	console.log('Somewhere, someone says hello.');
+});
+
+emtr.on('greet', function () {
+	console.log('A greeting occurred!');
+});
+
+console.log('Hello!');
+emtr.emit('greet');
+
+
+
+
+
+// object properties and menthods
+var obj = {
+	greet: 'Hello'
+}
+console.log(obj.greet);
+console.log(obj['greet']);
+var prop = 'greet';
+console.log(obj[prop]);
+
+// functions and arrays
+var arr = [];
+arr.push(function () {
+	console.log('Hello world 1');
+});
+arr.push(function () {
+	console.log('Hello world 2');
+});
+arr.push(function () {
+	console.log ('Hello world 3');
+});
+
+arr.forEach(function (item) {
+	item();
+});
+
+
+
+
+// core module
+var util = require('util');
+
+var name = 'Tony';
+var greeting = util.format('Hello, %s', name);
+util.log(greeting);
+
+
+
+
+
+var greet6 = require('./greet6');
+var greet7 = require('./greet7')
+greet7.greet();
+
 var greet = require('./greet');
 greet.english();
 greet.spanish();
@@ -21,3 +126,6 @@ grtr.greet();
 
 var greet5 = require('./greet5').greet;
 greet5();
+
+
+*/
